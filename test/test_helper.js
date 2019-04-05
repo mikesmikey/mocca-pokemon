840accbe3b983ad2096da07090c7dc48a9388cff
Kraiwich Kornsri <42442245+mikesmikey@users.noreply.github.com>
2019-04-05 12:02:41 +0700

    finish mocha test.

---
+++ test/test_helper.js
@@ -0,0 +1,17 @@
+//inside tests/test_helper.js
+const mongoose = require('mongoose');
+//tell mongoose to use es6 implementation of promises
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://jeff:jeff123@ds021326.mlab.com:21326/mocca-pokemon'); 
+mongoose.connection
+    .once('open', () => console.log('Connected!'))
+    .on('error', (error) => {
+        console.warn('Error : ',error);
+    });
+//Called hooks which runs before something.
+beforeEach((done) => {
+    mongoose.connection.collections.pokemons.drop(() => {
+         //this function runs after the drop is completed
+        done(); //go ahead everything is done now.
+    }); 
+});
